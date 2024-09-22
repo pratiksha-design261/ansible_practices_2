65d4db528849f664d6adb1b451290ab6add8361c
Ubuntu <ubuntu@ip-172-31-21-251.us-east-2.compute.internal>
2024-09-22 10:35:07 +0000

    all ansible topics related projects

---
+++ ansible_installation.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+ansible --version
+#IN VS code download below extension by redhat
+#yaml
+#ansible
